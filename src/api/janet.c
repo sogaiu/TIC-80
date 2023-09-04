9fe4a4154cc6d65000d821df7b03971e286394ea
sogaiu <983021772@users.noreply.github.com>
2023-09-04 11:54:24 +0900

    Try to report location for janet compile errors

--- src/api/janet.c
+++ src/api/janet.c
@@ -1047,6 +1047,104 @@ static void closeJanet(tic_mem* tic)
     }
 }
 
+int tic80_janet_dostring_ext(JanetTable *env, const char *str, const char *sourcePath, Janet *out) {
+    // from janet_dostring
+    int32_t len = 0;
+    while (str[len]) ++len;
+    const uint8_t *bytes = (const uint8_t *)str;
+
+    JanetParser parser;
+    int errflags = 0, done = 0;
+    int32_t index = 0;
+    Janet ret = janet_wrap_nil();
+    const uint8_t *where = sourcePath ? janet_cstring(sourcePath) : NULL;
+
+    if (where) janet_gcroot(janet_wrap_string(where));
+    if (NULL == sourcePath) sourcePath = "<unknown>";
+    janet_parser_init(&parser);
+
+    /* While we haven't seen an error */
+    while (!done) {
+
+        /* Evaluate parsed values */
+        while (janet_parser_has_more(&parser)) {
+            Janet form = janet_parser_produce(&parser);
+            JanetCompileResult cres = janet_compile(form, env, where);
+            if (cres.status == JANET_COMPILE_OK) {
+                JanetFunction *f = janet_thunk(cres.funcdef);
+                JanetFiber *fiber = janet_fiber(f, 64, 0, NULL);
+                fiber->env = env;
+                JanetSignal status = janet_continue(fiber, janet_wrap_nil(), &ret);
+                if (status != JANET_SIGNAL_OK && status != JANET_SIGNAL_EVENT) {
+                    janet_stacktrace_ext(fiber, ret, "");
+                    errflags |= 0x01;
+                    done = 1;
+                }
+            } else {
+                ret = janet_wrap_string(cres.error);
+                if (cres.macrofiber) {
+                    janet_eprintf("%d:%d: compile error in %s: ",
+                                  parser.line,
+                                  parser.column,
+                                  sourcePath);
+                    janet_stacktrace_ext(cres.macrofiber, ret, "");
+                } else {
+                    janet_eprintf("%d:%d: compile error in %s: %s\n",
+                                  parser.line,
+                                  parser.column,
+                                  sourcePath,
+                                  (const char *)cres.error);
+                }
+                errflags |= 0x02;
+                done = 1;
+            }
+        }
+
+        if (done) break;
+
+        /* Dispatch based on parse state */
+        switch (janet_parser_status(&parser)) {
+            case JANET_PARSE_DEAD:
+                done = 1;
+                break;
+            case JANET_PARSE_ERROR: {
+                const char *e = janet_parser_error(&parser);
+                errflags |= 0x04;
+                ret = janet_cstringv(e);
+                int32_t line = (int32_t) parser.line;
+                int32_t col = (int32_t) parser.column;
+                janet_eprintf("%s:%d:%d: parse error: %s\n", sourcePath, line, col, e);
+                done = 1;
+                break;
+            }
+            case JANET_PARSE_ROOT:
+            case JANET_PARSE_PENDING:
+                if (index >= len) {
+                    janet_parser_eof(&parser);
+                } else {
+                    janet_parser_consume(&parser, bytes[index++]);
+                }
+                break;
+        }
+
+    }
+
+    /* Clean up and return errors */
+    janet_parser_deinit(&parser);
+    if (where) janet_gcunroot(janet_wrap_string(where));
+#ifdef JANET_EV
+    /* Enter the event loop if we are not already in it */
+    if (janet_vm.stackn == 0) {
+        janet_gcroot(ret);
+        janet_loop();
+        janet_gcunroot(ret);
+    }
+#endif
+    if (out) *out = ret;
+    return errflags;
+}
+
+
 static bool initJanet(tic_mem* tic, const char* code)
 {
     closeJanet(tic);
@@ -1073,7 +1171,7 @@ static bool initJanet(tic_mem* tic, const char* code)
     Janet result;
 
     // Load the game source code
-    if (janet_dostring(core->currentVM, code, "main", &result)) {
+    if (tic80_janet_dostring_ext(core->currentVM, code, "main", &result)) {
         reportError(core, result);
         return false;
     }
@@ -1087,7 +1185,7 @@ static void evalJanet(tic_mem* tic, const char* code)
   tic_core* core = (tic_core*)tic;
 
   Janet result = janet_wrap_nil();
-  if (janet_dostring(core->currentVM, code, "main", &result)) {
+  if (tic80_janet_dostring_ext(core->currentVM, code, "main", &result)) {
       reportError(core, result);
   }
 }
